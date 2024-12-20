90d08f3aa38c8e5c499b0fa38dcf41ada4625519
rCotche <roshan.cotche@gmail.com>
2024-12-20 09:32:32 +0400

    audio added

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
+	animation_player.play("pickup")
+	
 	#simply remove the entire coin scene from our game
-	queue_free()
+	#queue_free()
