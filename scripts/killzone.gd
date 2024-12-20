5b144387773a160b4cfcf534e5db9a92afe5cdb1
rCotche <roshan.cotche@gmail.com>
2024-12-20 07:57:24 +0400

    dying 2.0

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,9 +4,16 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died!")
+	#slow the time in our game
+	Engine.time_scale = 0.5
+	#body fait reference à notre player
+	#car il y a que le player qui collision avec
+	#queue_free() = remove le node
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	#restart our game
 	get_tree().reload_current_scene()
