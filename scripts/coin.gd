bc3e8efb4447b0c6b009ed2228674db834407c8b
rCotche <roshan.cotche@gmail.com>
2024-12-20 09:05:59 +0400

    score added

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
-
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin")
+	game_manager.add_point()
 	#simply remove the entire coin scene from our game
 	queue_free()
