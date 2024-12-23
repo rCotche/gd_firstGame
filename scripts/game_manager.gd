7a4850f827a4534b606372a1c958b1aacc271948
rCotche <roshan.cotche@gmail.com>
2024-12-23 15:57:19 +0400

    create multiplayer button

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,3 +6,13 @@ var score = 0
 func add_point():
 	score +=1
 	score_label.text = "You collected " + str(score) + " coins."
+	
+func become_host():
+	print("host")
+	%MultiplayerHUD.hide()
+	MultiplayerManager.become_host()
+	
+func join_as_player_2():
+	print("join as player 2")
+	%MultiplayerHUD.hide()
+	MultiplayerManager.join_as_player_2()
---
+++ scripts/multiplayer/multiplayer_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func become_host():
+	print("starting host!")
+	
+func join_as_player_2():
+	print("join as player 2")
