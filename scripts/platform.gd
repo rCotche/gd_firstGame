9ca248960553f5905d0e6083d1e3e206fc27865c
rCotche <roshan.cotche@gmail.com>
2024-12-27 09:07:26 +0400

    sync platforms done

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -1,11 +1,13 @@
 extends AnimatableBody2D
 
+@export var animation_player_optional : AnimationPlayer
+
+func _on_player_connected(id):
+	if not multiplayer.is_server():
+		animation_player_optional.stop()
+		animation_player_optional.set_active(false)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if animation_player_optional:
+		multiplayer.peer_connected.connect(_on_player_connected)
